eb17ef41a642bfed8cfa8e3c813812a48c6ec0e0
Lusidiahwiranti <91247273+Lusidiahwiranti@users.noreply.github.com>
2023-04-17 00:28:09 +0700

    Update Album.vue

---
+++ src/views/Album.vue
@@ -0,0 +1,61 @@
+<template>
+  <section id = "sec-7" class = "py-5">
+
+    <div class = "container my-4">
+      <div class = "row gx-md-5">
+        <div class = "col-md-6 my-3">
+          <div class = "card text-center">
+            <img class = "card-img-top" src = "../assets/kecil.jpg">
+            <div class = "card-body py-5">
+              <h4 class = "card-title">Foto Kecil</h4>
+              <p class = "card-text py-3 lh-lg text-muted fw-light">Foto diatas ketika saya berumur 5 tahun. Foto tersebut diambil ketika acara khitanan abang saya.</p>
+            </div>
+          </div>
+        </div>
+
+        <div class = "col-md-6 my-3">
+          <div class = "card text-center">
+            <img class = "card-img-top" src = "../assets/smp.jpg">
+            <div class = "card-body py-5">
+              <h4 class = "card-title">Perpisahan SMP Negeri 9 Mandau</h4>
+              <p class = "card-text py-3 lh-lg text-muted fw-light">Foto diatas merupakan foto ketika acara perpisahan SMP Negeri 9 Mandau. Di acara perpisahan tersebut, saya beserta teman-teman saya menari Nirmala.</p>
+            </div>
+          </div>
+        </div>
+
+        <div class = "col-md-6 my-3">
+          <div class = "card text-center">
+            <img class = "card-img-top" src = "../assets/sma.jpg">
+            <div class = "card-body py-5">
+              <h4 class = "card-title">Pentas Seni SMAN 4 Mandau</h4>
+              <p class = "card-text py-3 lh-lg text-muted fw-light">Pentas seni merupakan kegiatan di SMA Negeri 4 Mandau yang diadakan setiap tahunnya. Pada foto diatas merupakan pentas seni yang diadakan pada tanggal 15 November 2019 saat saya kelas 2 SMA. Di pensi tersebut, saya beserta teman-teman saya mewakili kelas mendapatkan juara 1 dari 32 perwakilan kelas. Kami membawakan tarian kreasi yaitu Lenggang Puan Melayu dan dikombinasikan dengan Tari Lukah.</p>
+            </div>
+          </div>
+        </div>
+
+        <div class = "col-md-6 my-3">
+          <div class = "card text-center">
+            <img class = "card-img-top" src = "../assets/kuliahh.jpg">
+            <div class = "card-body py-5">
+              <h4 class = "card-title">Pengurus HIMATIF Periode 2023</h4>
+              <p class = "card-text py-3 lh-lg text-muted fw-light">Pengurus HIMATIF(Himpunan Mahasiswa Teknik Informatika) merupakan organisasi kepengurusan di jurusan Teknik Informatika. Foto diatas merupakan Pengurus HIMATIF Periode 2023. Pada tahun ini merupakan tahun kedua saya menjadi pengurus di Himpunan Mahasiswa Teknik Informatika.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
